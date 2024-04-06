b5afb8e4963dfbcb390b90342a5238baeaf57bf9
Sabrina-Prity <118998907+Sabrina-Prity@users.noreply.github.com>
2024-04-06 23:17:00 +0600

    Add files via upload
    added week4 day2

---
+++ Week-4/Day-1/Day-2/R_Remove_Prefix.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        vector<int> b;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        map<int, int> mp;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            mp[a[i]]++;
+
+            if (mp[a[i]] > 1)
+            {
+                break;
+            }
+            else
+            {
+                b.push_back(a[i]);
+            }
+        }
+        int m = b.size();
+        cout << n - m << endl;
+    }
+
+    return 0;
+}
