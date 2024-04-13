1efa9442867e7e5bec375d06bcdc1928095c4a7b
Sabrina-Prity <118998907+Sabrina-Prity@users.noreply.github.com>
2024-04-14 00:03:28 +0600

    Update basic_operations.cpp

--- Week-4/Day-1/Bit_Manipulation_1/basic_operations.cpp
+++ Week-4/Day-1/Bit_Manipulation_1/basic_operations.cpp
@@ -1,25 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int check_Kth_bit_on_or_off(int x,int k)
+int check_Kth_bit_on_or_off(int x, int k)
 {
-    return(x >> k) & 1;
+    return (x >> k) & 1;
 }
 
 void print_on_and_off_bits(int x)
 {
-    for(int k=0; k<=31; k++)
+    for (int k = 0; k <= 31; k++)
     {
-        if(check_Kth_bit_on_or_off(x,k))
+        if (check_Kth_bit_on_or_off(x, k))
         {
-            //if OFF print 1
+            // if OFF print 1
             cout << 1 << " ";
         }
         else
         {
-            //if OFF print 0
+            // if OFF print 0
             cout << 0 << " ";
-        }   
+        }
     }
     cout << '\n';
 }
@@ -28,21 +28,21 @@ void count_on_and_off_bits(int x)
 {
     int ON = 0;
     int OFF = 0;
-    for(int k=0; k<=31; k++)
+    for (int k = 0; k <= 31; k++)
     {
-        if(check_Kth_bit_on_or_off(x,k))
+        if (check_Kth_bit_on_or_off(x, k))
         {
-            //if ON print 1
+            // if ON print 1
             ON++;
         }
         else
         {
-            //if OFF print 0
+            // if OFF print 0
             OFF++;
-        }   
+        }
     }
-    cout << "ON bit -> " << ON <<'\n';
-    cout << "OFF bit -> " << OFF <<'\n';
+    cout << "ON bit -> " << ON << '\n';
+    cout << "OFF bit -> " << OFF << '\n';
 }
 
 int turn_on_Kth_bit(int x, int k)
@@ -55,7 +55,13 @@ int turn_off_Kth_bit(int x, int k)
     return x & (~(1 << k));
 }
 
-int main(){
+int toggle_Kth_bit(int x, int k)
+{
+    return (x ^ (1 << k));
+}
+
+int main()
+{
 
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -67,7 +73,10 @@ int main(){
     // count_on_and_off_bits(44);
 
     // cout << turn_on_Kth_bit(44,4);
-    cout << turn_off_Kth_bit(44,3);
+    // cout << turn_off_Kth_bit(44, 3);
+
+    // cout << toggle_Kth_bit(44,3);
+    cout << toggle_Kth_bit(44,4);
 
     return 0;
 }
