89c1f8aa9b141a7f40a9f225dc955f164ebb9b74
Sabrina-Prity <118998907+Sabrina-Prity@users.noreply.github.com>
2024-04-27 23:36:09 +0600

    Add files via upload
    added week3 day1

---
+++ Week-3/Day-2/Day-1/A_Is_It_a_Cat.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        string s;
+        cin >> n >> s;
+        for (auto &x : s)
+        {
+            x = tolower(x);
+        }
+        auto it = unique(s.begin(), s.end());
+
+        s.resize(distance(s.begin(), it));
+        // cout << s << endl;
+        string s2 = "meow";
+        (s == s2) ? cout << "YES" << '\n' : cout << "NO" << '\n';
+        // cout << s << " " << s1 << " " << s2 << endl;
+    }
+
+    return 0;
+}
