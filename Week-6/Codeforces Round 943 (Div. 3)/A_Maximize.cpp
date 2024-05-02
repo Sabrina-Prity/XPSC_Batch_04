543e6bd0fb5d2750043e69b9a19e42d263841ab9
Sabrina-Prity <118998907+Sabrina-Prity@users.noreply.github.com>
2024-05-02 22:51:23 +0600

    Add files via upload

---
+++ Week-6/Codeforces Round 943 (Div. 3)/A_Maximize.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// int gcd(int a, int b) 
+// { 
+//     int result = min(a, b); 
+//     while (result > 0) { 
+//         if (a % result == 0 && b % result == 0) { 
+//             break; 
+//         } 
+//         result--; 
+//     } 
+
+//     return result; 
+// } 
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int x;
+        cin >> x;
+
+       cout << x-1 << '\n';
+    }
+
+    return 0;
+}
