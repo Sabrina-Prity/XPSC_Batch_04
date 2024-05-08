fe4589c4caddaffed75a9a479a0d465aa240d38c
Sabrina-Prity <118998907+Sabrina-Prity@users.noreply.github.com>
2024-05-09 00:04:39 +0600

    Update M_Deja_Vu.cpp

--- Week-7/Day-6/M_Deja_Vu.cpp
+++ Week-7/Day-6/M_Deja_Vu.cpp
@@ -1,46 +1,46 @@
 #include<bits/stdc++.h>
+#define ll long long
 using namespace std;
-int main(){
 
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
     int t;
     cin >> t;
-    while(t--)
-    {
-        int n,q;
+    while(t--) {
+        ll n, q;
         cin >> n >> q;
-        vector<int> a(n);
-        for(int i=0; i<n; i++)
-        {
+        vector<ll> a(n);
+        for(int i = 0; i < n; i++) {
             cin >> a[i];
         }
 
-        vector<int>b(q);
-        for(int i=0; i<q; i++)
-        {
+        vector<ll> b(q);
+        for(int i = 0; i < q; i++) {
             cin >> b[i];
         }
-    
-            for(int i=0; i<n; i++)
+
+        set<ll> st;
+        for(int i = 0; i < q; i++) {
+            if(st.count(b[i]))
             {
-                for(auto it : b)
-                {
-                    if(a[i]%(1<<it) == 0)
-                    {
-                        it -= 1;
-                        a[i] += (1<<it);
-                    }
-                }
-              
-            }  
-            for(auto it : a)
-            {
-                cout << it << " ";
+                continue;
             }
-            cout << '\n';
+            st.insert(b[i]);
+            for(int j = 0; j < n; j++) {
+                if(a[j] % (1LL << b[i]) == 0) 
+                {
+                    a[j] += (1LL << (b[i] - 1)); 
+                }
+            }
+        }
         
+
+        for(auto it : a) {
+            cout << it << " ";
+        }
+        cout << '\n';
     }
 
     return 0;
