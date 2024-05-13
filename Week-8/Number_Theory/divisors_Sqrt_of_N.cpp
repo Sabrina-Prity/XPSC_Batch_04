c017342576f08227b74296230e2df2602ec21737
Sabrina-Prity <118998907+Sabrina-Prity@users.noreply.github.com>
2024-05-13 13:20:12 +0600

    Add files via upload

---
+++ Week-8/Number_Theory/divisors_Sqrt_of_N.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<int> divisors;
+    for(int i=1; i*i<=n; i++)
+    {
+        if(n%i == 0)
+        {
+            divisors.push_back(i);
+            if((n/i) != i)
+            {
+                divisors.push_back(n/i);
+            }
+        }
+    }
+
+    sort(divisors.begin(), divisors.end());
+
+    for(auto x: divisors)
+    {
+        cout << x << " ";
+    }
+
+    return 0;
+}
