0f8eebbcde5f028a58d2070170559555553f8054
Sabrina-Prity <118998907+Sabrina-Prity@users.noreply.github.com>
2024-07-13 20:15:27 +0600

    Update K_Great_Sequence.cpp

--- Week-15/Day-5/K_Great_Sequence.cpp
+++ Week-15/Day-5/K_Great_Sequence.cpp
@@ -21,25 +21,28 @@ int main()
         sort(a.begin(), a.end());
         reverse(a.begin(), a.end());
         
-        map<int, int>m;
+        map<ll, int> m;
         for (int i = 0; i < n; i++)
         {
-            m[a[i]]+=1;
+            m[a[i]] += 1;
         }
-        // rep(i, n)cout<<m1[x*a[i]]<<endl;
         int ans = 0;
-        for(int i = n-1;i>=0;i--){
-            if(m[a[i]]){
-            if(m[x*a[i]]){
-                m[x*a[i]]--;
-            }
-            else{
-                ans++;
-            }
-            m[a[i]]--;
+        for (int i = n - 1; i >= 0; i--) 
+        {
+            if (m[a[i]]) 
+            {
+                if (m[x * a[i]]) 
+                {
+                    m[x * a[i]]--;
+                } 
+                else 
+                {
+                    ans++;
+                }
+                m[a[i]]--;
             }
         }
-        cout<<ans<<endl;
+        cout << ans << endl;
     }
 
     return 0;
